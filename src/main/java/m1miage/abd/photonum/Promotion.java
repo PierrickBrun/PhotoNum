9ef6fd9a57727d796ff2cbc28ab4275edffce4e6
Pierrick Brun <pierrickjabrun@gmail.com>
2016-01-13 15:06:42 +0100

    Photo et Promotion

--- src/main/java/m1miage/abd/photonum/Promotion.java
+++ src/main/java/m1miage/abd/photonum/Promotion.java
@@ -2,7 +2,29 @@ package m1miage.abd.photonum;
 
 public class Promotion {
 
-	private Commande cmdDelivre ;
-	private Commande comdUtilise ;
-	private int montant ;
+	private Commande cmdDelivre;
+	private Commande comdUtilise;
+	private float montant;
+	
+	public Promotion(Commande comDelivre){
+		this.cmdDelivre = comDelivre;
+		float mnt = comDelivre.getPrixTotal();
+		this.montant = (5/100)*mnt;
+	}
+
+	public Commande getComdUtilise() {
+		return comdUtilise;
+	}
+
+	public void setComdUtilise(Commande comdUtilise) {
+		this.comdUtilise = comdUtilise;
+	}
+
+	public float getMontant() {
+		return montant;
+	}
+
+	public Commande getCmdDelivre() {
+		return cmdDelivre;
+	}
 }
