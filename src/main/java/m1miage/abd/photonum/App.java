63205d7cd3c1bdb90eb73812f6f51611a5b97598
AissaA <aissa.a.26@gmail.com>
2016-01-26 18:33:00 +0100

    test

--- src/main/java/m1miage/abd/photonum/App.java
+++ src/main/java/m1miage/abd/photonum/App.java
@@ -1,9 +1,13 @@
 package m1miage.abd.photonum;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import m1miage.abd.photonum.model.Album;
 import m1miage.abd.photonum.model.Client;
+import m1miage.abd.photonum.db.*;
 import util.ConnectDatabase;
 
 /**
@@ -15,10 +19,27 @@ public class App {
 	public static void main(String[] args) {
 
 		Connection db = ConnectDatabase.getConnection();
+		db = ConnectDatabase.getConnection();
+		db = ConnectDatabase.getConnection();
 		
-		/*Client client = new Client("  ", "  ", "  ", "  ", "  ");
+		
+		//Connection conn = ConnectDatabase.getConnection();
+		ClientDAO client = new ClientDAO(db);
+		Statement statement;
+		try {
+			statement = db.createStatement();
+			ResultSet rs = statement.executeQuery("select * from Client");
+			System.out.println(statement);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		//client.getAllUsers();
+	
+		//Client client = new Client("  ", "  ", "  ", "  ", "  ");
 
-		Album album = new Album(client, "  ", "  ", 0);
-		System.out.println(album);*/
+		//Album album = new Album(client, "  ", "  ", 0);
+		//System.out.println(album);*/
 	}
 }
