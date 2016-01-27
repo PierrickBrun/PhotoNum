80ec82803fff38758a2e40e245c32cc0422d505c
AissaA <aissa.a.26@gmail.com>
2016-01-27 16:11:59 +0100

    clientDAO

--- src/main/java/m1miage/abd/photonum/db/ClientDAO.java
+++ src/main/java/m1miage/abd/photonum/db/ClientDAO.java
@@ -23,15 +23,18 @@ public class ClientDAO {
 
 	 public void addClient(Client user) {
 		 try {
-	            PreparedStatement preparedStatement = connection.prepareStatement("insert into Client(NOM,PRENOM,MAIL,MOT_DE_PASSE) values (?, ?, ?, ? )");
+	            PreparedStatement preparedStatement = connection.prepareStatement("insert into Client(NOM,PRENOM,MAIL,ADRESSE,MOT_DE_PASSE) values (?, ?, ?, ?, ? )");
 
 	            // Parameters start with 1
-	            preparedStatement.setString(2, user.getNom());
-	            preparedStatement.setString(1, user.getPrenom());	            
+	            
+	            preparedStatement.setString(1, user.getNom());
+	            preparedStatement.setString(2, user.getPrenom());	            
 	            preparedStatement.setString(3, user.getMail());	            
 	            preparedStatement.setString(4, user.getAdresse());
 	            preparedStatement.setString(5, user.getMdp());
 	            preparedStatement.executeUpdate();
+	            System.out.println("Le client "+user.getNom()+" a bien été ajouté");
+	            
 	        } catch (SQLException e) {
 	            e.printStackTrace();
 	        }
@@ -53,21 +56,41 @@ public class ClientDAO {
 
 	    public void updateUser(Client user) {
 	        try {
-	            PreparedStatement preparedStatement = connection.prepareStatement("update CLIENT set NOM=?, PRENOM=?, MAIL=?, MOT_DE_PASSE=?" +"where userid=?");
+	            PreparedStatement preparedStatement = connection.prepareStatement("update CLIENT set NOM=?, PRENOM=?, MAIL=?, ADRESSE=?, MOT_DE_PASSE=? where IDCLIENT=?");
 	            // Parameters start with 1
-	            preparedStatement.setInt(1, user.getIdClient());
-	            preparedStatement.setString(2, user.getNom());
-	            preparedStatement.setString(3, user.getPrenom());
-	            preparedStatement.setString(4, user.getMail());	            
-	            preparedStatement.setString(5, user.getAdresse());
-	            preparedStatement.setString(6, user.getMdp());
+	            
+	            preparedStatement.setString(1, user.getNom());
+	            preparedStatement.setString(2, user.getPrenom());
+	            preparedStatement.setString(3, user.getMail());	            
+	            preparedStatement.setString(4, user.getAdresse());
+	            preparedStatement.setString(5, user.getMdp());
+	            preparedStatement.setInt(6, user.getIdClient());
 	            preparedStatement.executeUpdate();
+	            System.out.println("Le client "+user.getNom()+" a bien été mis à jour");
 	        } catch (SQLException e) {
 	            e.printStackTrace();
 	        }
 	    }
 
-
+	    public void majID(Client user){
+	    	Statement statement;
+			try {
+				statement = connection.createStatement();
+				ResultSet rs = statement.executeQuery("select * from Client");
+				if (rs.next()) {	
+	            	 user.setIdClient(rs.getInt("IDCLIENT"));
+		             user.setNom(rs.getString("NOM"));
+		             user.setPrenom(rs.getString("PRENOM"));
+		             user.setMail(rs.getString("MAIL"));
+		             user.setAdresse(rs.getString("ADRESSE"));
+		             user.setMdp(rs.getString("MOT_DE_PASSE"));
+	            }
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	    }
 
 
 	    public List<Client> getAllUsers() {
@@ -96,7 +119,7 @@ public class ClientDAO {
 	    public Client getUserById(int userId) {
 	        Client user = new Client();
 	        try {
-	            PreparedStatement preparedStatement = connection.prepareStatement("select * from CLIENT where IDCLIENT="+userId);
+	            PreparedStatement preparedStatement = connection.prepareStatement("select * from CLIENT where IDCLIENT=?");
 	            preparedStatement.setInt(1, userId);
 	            ResultSet rs = preparedStatement.executeQuery();
 	            if (rs.next()) {	
@@ -106,6 +129,28 @@ public class ClientDAO {
 		             user.setMail(rs.getString("MAIL"));
 		             user.setAdresse(rs.getString("ADRESSE"));
 		             user.setMdp(rs.getString("MOT_DE_PASSE"));
+		             System.out.println("Client trouvé :"+user.getNom());
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return user;
+	    }
+	    
+	    public Client getUserByMail(String userMail) {
+	        Client user = new Client();
+	        try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("select * from CLIENT where MAIL=?");
+	            preparedStatement.setString(1, userMail);
+	            ResultSet rs = preparedStatement.executeQuery();
+	            if (rs.next()) {	
+	            	 user.setIdClient(rs.getInt("IDCLIENT"));
+		             user.setNom(rs.getString("NOM"));
+		             user.setPrenom(rs.getString("PRENOM"));
+		             user.setMail(rs.getString("MAIL"));
+		             user.setAdresse(rs.getString("ADRESSE"));
+		             user.setMdp(rs.getString("MOT_DE_PASSE"));
+		             System.out.println("Client trouvé :"+user.getNom());
 	            }
 	        } catch (SQLException e) {
 	            e.printStackTrace();
