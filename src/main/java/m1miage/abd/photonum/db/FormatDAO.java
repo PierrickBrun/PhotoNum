338a388355f7537d5b4bc05d2f9372b1c6097f7d
Laura <Laura@pc-Laura>
2016-01-27 13:12:41 +0100

    FormatDAO

--- src/main/java/m1miage/abd/photonum/db/FormatDAO.java
+++ src/main/java/m1miage/abd/photonum/db/FormatDAO.java
@@ -1,5 +1,107 @@
 package m1miage.abd.photonum.db;
 
-public class FormatDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import m1miage.abd.photonum.model.Format;
+
+public class FormatDAO {
+	 private Connection connection;
+
+	 public FormatDAO(Connection conn) {
+		 this.connection = conn;
+	    }
+
+	 public void addFormat(Format format) {
+		 try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("insert into FORMAT (LIBELLE, PRIXUNITAIRE, RESOLUTIONMINIMALE, VITESSEIMPRESSION, STOCKPAPIER) values (?, ?, ?, ?, ? )");
+	            // Parameters start with 1
+	  // IDFORMAT
+	            preparedStatement.setString(2, format.getLibelle());
+	            preparedStatement.setFloat(3, format.getPrixUnitaire());
+	            preparedStatement.setString(4, format.getResolutionMinimale());	            
+	            preparedStatement.setFloat(5, format.getVitesseImpression());	            
+	            preparedStatement.setInt(6, format.getStockPapier());
+	            preparedStatement.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+
+	    public void deleteFormat (int formatId) {
+	        try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("delete from FORMAT where IDFORMAT="+formatId);
+	            // Parameters start with 1
+	            preparedStatement.setInt(1, formatId);
+	            preparedStatement.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+	    public void updateFormat(Format format) {
+	        try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("update FORMAT set LIBELLE=?, PRIXUNITAIRE=?, RESOLUTIONMINIMALE=?, VITESSEIMPRESSION=?, STOCKPAPIER=?" +"where IDFORMAT=?");
+	            // Parameters start with 1
+	            preparedStatement.setString(2, format.getLibelle());
+	            preparedStatement.setFloat(3, format.getPrixUnitaire());
+	            preparedStatement.setString(4, format.getResolutionMinimale());	            
+	            preparedStatement.setFloat(5, format.getVitesseImpression());	            
+	            preparedStatement.setInt(6, format.getStockPapier());
+	            preparedStatement.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+
+
+	    public List<Format> getAllFormat() {
+	    	List<Format> formats = new ArrayList<Format>();
+	        try {
+	            Statement statement = connection.createStatement();
+	            ResultSet rs = statement.executeQuery("select * from COMMANDE");
+	            while (rs.next()) {
+	            	Format format = new Format();
+	            	format.setIdFormat(rs.getInt("IDFORMAT"));
+	            	format.setLibelle(rs.getString("LIBELLE"));
+	            	format.setPrixUnitaire(rs.getFloat("PRIXUNITAIRE"));
+	            	format.setResolutionMinimale(rs.getString("RESOLUTIONMINIMALE"));
+	            	format.setVitesseImpression(rs.getFloat("VITESSEIMPRESSION"));
+	                format.setStockPapier(rs.getInt("STOCKPAPIER"));
+	                formats.add(format);              
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return formats;
+	    }
+
+	    public Format getFormatById(int formatId) {
+	        Format format = new Format();
+	        try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("select * from FORMAT where IDFORMAT="+formatId);
+	            //preparedStatement.setInt(1, userId);
+	            ResultSet rs = preparedStatement.executeQuery();
+	            if (rs.next()) {	            	
+	            	format.setLibelle(rs.getString("LIBELLE"));
+	            	format.setPrixUnitaire(rs.getFloat("PRIXUNITAIRE"));
+	            	format.setResolutionMinimale(rs.getString("RESOLUTIONMINIMALE"));
+	            	format.setVitesseImpression(rs.getFloat("VITESSEIMPRESSION"));
+	                format.setStockPapier(rs.getInt("STOCKPAPIER"));
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return format;
+	    }
 }
