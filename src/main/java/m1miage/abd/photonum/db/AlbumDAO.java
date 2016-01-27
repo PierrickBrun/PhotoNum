8ab125d4d6f9e3eecf5d9cd68c37650101947732
Laura <Laura@pc-Laura>
2016-01-27 11:13:27 +0100

    AlbumDAO et modif Article

--- src/main/java/m1miage/abd/photonum/db/AlbumDAO.java
+++ src/main/java/m1miage/abd/photonum/db/AlbumDAO.java
@@ -1,5 +1,116 @@
 package m1miage.abd.photonum.db;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import m1miage.abd.photonum.model.Album;
+
+import  util.DatabaseAccessProperties;
 
 public class AlbumDAO {
 
+	 private Connection connection;
+
+	 public AlbumDAO(Connection conn) {
+		 this.connection = conn;
+	    }
+
+	 public void addAlbum(Album album) {
+		 try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("insert into Album(IDCLIENT, TITRE, PREFACE, POSTFACE, NBPAGE, TYPE) values (?, ?, ?, ?, ?, ? )");
+
+	            // Parameters start with 1
+	  // IDALBUM
+	            preparedStatement.setInt(2, album.getClient().getIdClient());
+	            preparedStatement.setString(3, album.getTitre());
+	            preparedStatement.setString(4, album.getPreface());	            
+	            preparedStatement.setString(5, album.getPostface());	            
+	            preparedStatement.setInt(6, album.getNbPage());
+	            preparedStatement.setString(7, album.getTypeAlbum());
+	            preparedStatement.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+
+	    public void deleteAlbum (int albumId) {
+	        try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("delete from Album where IDALBUM="+albumId);
+	            // Parameters start with 1
+	            preparedStatement.setInt(1, albumId);
+	            preparedStatement.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+	    public void updateAlbum(Album album) {
+	        try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("update ALBUM set IDCLIENT=?, TITRE=?, PREFACE=?, POSTFACE=?, NBPAGE=?, TYPE=?" +"where IDALBUM=?");
+	            // Parameters start with 1
+	            preparedStatement.setInt(2, album.getClient().getIdClient());
+	            preparedStatement.setString(3, album.getTitre());
+	            preparedStatement.setString(4, album.getPreface());	            
+	            preparedStatement.setString(5, album.getPostface());	            
+	            preparedStatement.setInt(6, album.getNbPage());
+	            preparedStatement.setString(7, album.getTypeAlbum());
+	            preparedStatement.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+
+
+	    public List<Album> getAllAlbum() {
+	    	List<Album> albums = new ArrayList<Album>();
+	        try {
+	            Statement statement = connection.createStatement();
+	            ResultSet rs = statement.executeQuery("select * from ALBUM");
+	            while (rs.next()) {
+	            	Album album = new Album();
+	            	album.setIdAlbum(rs.getInt("IDALBUM"));
+	            	album.getClient().setIdClient(rs.getInt("IDCLIENT"));
+	            	album.setTitre(rs.getString("TITRE"));
+	            	album.setPreface(rs.getString("PREFACE"));
+	            	album.setPostface(rs.getString("POSTFACE"));
+	                album.setNbPage(rs.getInt("NBPAGE"));
+	                album.affecterType(rs.getString("TYPE"));
+	                albums.add(album);              
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return albums;
+	    }
+
+
+
+	    public Album getAlbumById(int albumId) {
+	        Album album = new Album();
+	        try {
+	            PreparedStatement preparedStatement = connection.prepareStatement("select * from ALBUM where IDALBUM="+albumId);
+	            //preparedStatement.setInt(1, userId);
+	            ResultSet rs = preparedStatement.executeQuery();
+	            if (rs.next()) {	            	
+	            	album.getClient().setIdClient(rs.getInt("IDCLIENT"));
+	            	album.setTitre(rs.getString("TITRE"));
+	            	album.setPreface(rs.getString("PREFACE"));
+	            	album.setPostface(rs.getString("POSTFACE"));
+	                album.setNbPage(rs.getInt("NBPAGE"));
+	                album.affecterType(rs.getString("TYPE"));
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return album;
+	    }
 }
--- src/main/java/m1miage/abd/photonum/model/Article.java
+++ src/main/java/m1miage/abd/photonum/model/Article.java
@@ -5,6 +5,8 @@ public class Article {
 	private int idArticle;
 	private Commande commande;
 	private Album album;
+	private Format format;
+	private Prestataire prestataire;
 	private int quantite;
 	private float prix;
 
@@ -34,6 +36,30 @@ public class Article {
 	public void setAlbum(Album album) {
 		this.album = album;
 	}
+	
+	public int getIdArticle() {
+		return idArticle;
+	}
+
+	public void setIdArticle(int idArticle) {
+		this.idArticle = idArticle;
+	}
+
+	public Format getFormat() {
+		return format;
+	}
+
+	public void setFormat(Format format) {
+		this.format = format;
+	}
+
+	public Prestataire getPrestataire() {
+		return prestataire;
+	}
+
+	public void setPrestataire(Prestataire prestataire) {
+		this.prestataire = prestataire;
+	}
 
 	public int getQuantite() {
 		return quantite;
@@ -47,8 +73,8 @@ public class Article {
 		return this.prix;
 	}
 
-	public int getId() {
-		return this.idArticle;
+	public void setPrix(float prix) {
+		this.prix = prix;
 	}
 
 }
